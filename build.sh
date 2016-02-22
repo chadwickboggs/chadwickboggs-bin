0022c23383808bf423ab9bb1d951e9e9e0b924f1
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-02-22 11:26:42 -0700

    Updates.

--- build.sh
+++ build.sh
@@ -2,41 +2,46 @@
 
 fast=false
 quiet=false
+silent=false
 
-USAGE="$(basename $0) <options> \n
-\n
-\t	Maven build the current folder. \n
-\n
-\t	Options \n
-\n
-\t\t		[-h|--help]\t\t\t\t		This message gets printed only. \n
-\t\t		[-f|--fast] <fast>\t	Do not clean and do skip tests.  Default: \"${fast}\" \n
-\t\t		[-q|--quiet] <quiet>\t	Suppress all but the final success and failure messages.  Default: \"${quiet}\" \n
+USAGE="$(basename $0) <options>
+
+\t	Maven build the current folder.
+
+\t	Options
+
+\t\t		[-h|--help]	This message gets printed only.
+\t\t		[-f|--fast]	Do not clean and do skip tests.  Default: \"${fast}\"
+\t\t		[-q|--quiet]	Suppress all but the final success and failure messages and stderr.  Default: \"${quiet}\"
+\t\t		[-s|--silent]	Suppress stderr.  Default: \"${silent}\"
 "
 
-args=`getopt -o "hfq" -l "help,fast,quiet" -- "$@"`
+args=`getopt -o "hfqs" -l "help,fast,quiet,silent" -- "$@"`
 eval set -- "$args"
 while true; do
   case "$1" in
-		-h | --help)	echo -e ${USAGE}; exit;;
+		-h | --help)	echo -e "${USAGE}"; exit;;
 		-f | --fast)	fast=true; shift;;
 		-q | --quiet)	quiet=true; shift;;
+		-s | --silent)	silent=true; shift;;
 		--) shift; break;;
 		*) echo "Internal error!"; exit 1;;
   esac
 done
 
 if [[ ${fast} == true ]]; then
-	if [[ ${quiet} == true ]]; then
-		mvn -e -P local install -DskipTests $@ | grep '\[INFO\] BUILD [SF]'
-	else
-		mvn -e -P local install -DskipTests $@
-	fi
+	cmd="mvn -e -P local install -DskipTests $@"
 else
-	if [[ ${quiet} == true ]]; then
-		mvn -e -U -P local clean install $@ | grep '\[INFO\] BUILD [SF]'
-	else
-		mvn -e -U -P local clean install $@
-	fi
+	cmd="mvn -e -U -P local clean install $@"
 fi
+
+if [[ ${silent} == true ]]; then
+	cmd="${cmd} 2> /dev/null"
+fi
+
+if [[ ${quiet} == true ]]; then
+	cmd="${cmd} | grep '\[INFO\] BUILD [SF]'"
+fi
+
+bash -c "${cmd}"
 
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+echo "[$(now)] Deploying..."
+
 #cp $@ target/*.war $CATALINA_HOME/webapps/.
 lspf "{} -name '*.war'" | parallel cp $@ "{}" $CATALINA_HOME/webapps/.
+
+exit_code=$?
+
+echo "[$(now)] Done deploying."
+
+exit ${exit_code}
 
