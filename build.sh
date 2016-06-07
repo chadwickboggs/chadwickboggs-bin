37b33b036e1e2de28921c7035f7c90862e2ebe78
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-06-07 11:22:46 -0600

    Updates.

--- build.sh
+++ build.sh
@@ -11,7 +11,10 @@ skip_update='-U'
 clean_build_logs=false
 #use_args='-e -P local'
 use_args='-e'
+failsafe_debug=false
 dryrun=false
+
+FAILSAFE_DEBUG_ARGS="-Dmaven.failsafe.debug"
 
 USAGE="$(basename $0) <options>
 
@@ -28,23 +31,25 @@ USAGE="$(basename $0) <options>
 \t\t		[-u|--skip_update]	Skip update.  Default: false
 \t\t		[-b|--clean_build_logs]	Clean build log files.  Default: ${clean_build_logs}
 \t\t		[-a|--use_args]		Add args to maven.  Default: \"${use_args}\"
+\t\t		[-d|--failsafe_debug]	Enable failesafe debug.  Default: \"${failsafe_debug}\"
 \t\t		[-n|--dryrun]		Print but do not execute.  Default: \"${dryrun}\"
 "
 
-args=`getopt -o "ha:fqsctunb" -l "help,use_args,clean_build_logs,fast,quiet,silent,skip_clean,skip_tests,skip_update,dryrun" -- "$@"`
+args=`getopt -o "ha:fqsctunbd" -l "help,use_args,clean_build_logs,fast,quiet,silent,skip_clean,skip_tests,skip_update,failsafe_debug,dryrun" -- "$@"`
 eval set -- "$args"
 while true; do
   case "$1" in
-		-h | --help)		echo -e "${USAGE}"; exit;;
-		-f | --fast)		fast=true; skip_tests="-DskipTests -Dmaven.test.skip=true"; shift;;
-		-q | --quiet)		quiet=true; shift;;
-		-s | --silent)		silent=true; shift;;
-		-c | --skip_clean)	skip_clean=true; shift;;
-		-t | --skip_tests)	skip_tests="-DskipTests -Dmaven.test.skip=true"; shift;;
-		-u | --skip_update)	skip_update=''; shift;;
+		-h | --help)				echo -e "${USAGE}"; exit;;
+		-f | --fast)				fast=true; skip_tests="-DskipTests -Dmaven.test.skip=true"; shift;;
+		-q | --quiet)				quiet=true; shift;;
+		-s | --silent)				silent=true; shift;;
+		-c | --skip_clean)			skip_clean=true; shift;;
+		-t | --skip_tests)			skip_tests="-DskipTests -Dmaven.test.skip=true"; shift;;
+		-u | --skip_update)			skip_update=''; shift;;
 		-b | --clean_build_logs)	clean_build_logs=true; shift;;
-		-a | --use_args)	shift; use_args="$1"; shift;;
-		-n | --dryrun)		dryrun=true; shift;;
+		-a | --use_args)			shift; use_args="$1"; shift;;
+		-d | --failsafe_debug)		failsafe_debug=true; shift;;
+		-n | --dryrun)				dryrun=true; shift;;
 		--) shift; break;;
 		*) echo "Internal error!"; exit 1;;
   esac
@@ -52,13 +57,17 @@ done
 
 [[ ${clean_build_logs} == true ]] && ${SCRIPT_HOME}/clean_build_logs.sh
 
+cmd="mvn"
+
+[[ ${failsafe_debug} == true ]] && cmd="${cmd} ${FAILSAFE_DEBUG_ARGS}"
+
 if [[ ${fast} == true ]]; then
-	cmd="mvn ${use_args} ${skip_tests} install $@"
+	cmd="${cmd} ${use_args} ${skip_tests} install $@"
 else
 	if [[ ${skip_clean} == true ]]; then
-		cmd="mvn ${use_args} ${skip_tests} ${skip_update} install $@"
+		cmd="${cmd} ${use_args} ${skip_tests} ${skip_update} install $@"
 	else
-		cmd="mvn ${use_args} ${skip_tests} ${skip_update} clean install $@"
+		cmd="${cmd} ${use_args} ${skip_tests} ${skip_update} clean install $@"
 	fi
 fi
 
