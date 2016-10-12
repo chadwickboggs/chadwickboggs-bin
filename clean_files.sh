058f706497dcd0c61c88ae19cf36fffcdbe2b3f0
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-10-12 16:30:16 -0600

    Updates.

--- clean_files.sh
+++ clean_files.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+[[ $# == 0 ]] && echo "Filenames must be specified on the command line.  Exiting." && exit 1
+
 files=$(find . -name "$*")
 
-[[ $(echo ${files} | wc -l) > 0 ]] && rm -v ${files}
+[[ $(echo -n ${files} | wc -l) == 0 ]] && exit 0
+
+rm -v ${files}
 
