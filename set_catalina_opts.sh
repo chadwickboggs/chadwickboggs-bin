b59083575968e1884caf873ebca2bce7e34de689
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-12-07 11:13:59 -0700

    Added git ignore.

--- set_catalina_opts.sh
+++ set_catalina_opts.sh
@@ -2,8 +2,8 @@
 
 export CATALINA_OPTS_ENV=$1
 
-export CATALINA_OPTS_JVM="-Xms1g -Xmx2g -server -XX:NewSize=256m -XX:MaxNewSize=512m -XX:+DisableExplicitGC -Dfile.encoding=UTF-8"
-[[ $(java_major_version.sh) < 8 ]] && CATALINA_OPTS_JVM="-XX:PermSize=512m -XX:MaxPermSize=256m ${CATALINA_OPTS_JVM}"
+#export CATALINA_OPTS_JVM="-Xms1g -Xmx2g -server -XX:NewSize=256m -XX:MaxNewSize=512m -XX:+DisableExplicitGC -Dfile.encoding=UTF-8"
+#[[ $(java_major_version.sh) < 8 ]] && CATALINA_OPTS_JVM="-XX:PermSize=512m -XX:MaxPermSize=256m ${CATALINA_OPTS_JVM}"
 
 export CATALINA_OPTS_AWT="-Djava.awt.headless=true"
 export CATALINA_OPTS_JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9095 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.password.file=conf/jmxremote.password -Dcom.sun.management.jmxremote.access.file=conf/jmxremote.access"
@@ -17,7 +17,8 @@ export CATALINA_OPTS_APP="-Dapplication.home=${HOME} -DlocalLogging=true -Daesd.
 	export CATALINA_OPTS_NFOSS_ENV="-Deureka.environment=${CATALINA_OPTS_ENV}"
 #fi
 
-export CATALINA_OPTS_CHARTER="${CATALINA_OPTS_JVM} ${CATALINA_OPTS_AWT} ${CATALINA_OPTS_JMX} ${CATALINA_OPTS_ARCHAIUS} ${CATALINA_OPTS_NFOSS} ${CATALINA_OPTS_APP} ${CATALINA_OPTS_LOGBACK_ENV} ${CATALINA_OPTS_ARCHAIUS_ENV} ${CATALINA_OPTS_NFOSS_ENV}"
+#export CATALINA_OPTS_CHARTER="${CATALINA_OPTS_JVM}"
+export CATALINA_OPTS_CHARTER="${CATALINA_OPTS_CHARTER} ${CATALINA_OPTS_AWT} ${CATALINA_OPTS_JMX} ${CATALINA_OPTS_ARCHAIUS} ${CATALINA_OPTS_NFOSS} ${CATALINA_OPTS_APP} ${CATALINA_OPTS_LOGBACK_ENV} ${CATALINA_OPTS_ARCHAIUS_ENV} ${CATALINA_OPTS_NFOSS_ENV}"
 export CATALINA_OPTS="${CATALINA_OPTS_CHARTER}"
 
 echo $CATALINA_OPTS
