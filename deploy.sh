b883fa289fbd2d873e3a452cf2be19d798d2d0ba
Chadwick Boggs <chadwickboggs@gmail.com>
2019-01-14 14:44:07 -0700

    Updates.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "[$(now)] Deploying..."
 
 #cp $@ target/*.war $CATALINA_HOME/webapps/.
-lspf -q "{} -name '*.war'" | grep -v cargo | parallel "cp $@ {} $CATALINA_HOME/webapps/."
+lspf -q "{} -name '*.war'" | grep -v cargo | parallel "cp -v $@ {} $CATALINA_HOME/webapps/."
 
 exit_code=$?
 
